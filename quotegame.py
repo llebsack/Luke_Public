81212ffc338009ac50d8646dde09f29d39bb3bd2
llebsack <lukelebsack8@gmail.com>
2023-11-17 23:34:54 -0800

    added conditional logic and guesses

--- quotegame.py
+++ quotegame.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
 all_quotes = []
 base_url= "https://quotes.toscrape.com"
@@ -22,5 +23,53 @@ while url:
 
     next_btn = soup.find(class_="next")
     url = next_btn.find("a")["href"] if next_btn else None
-    sleep(1)
-print(all_quotes)
+
+def start_game():
+    quote = choice(all_quotes)
+    remaining_guesses = 4
+    print("Here's a quote: ")
+    print(quote["text"])
+    print(quote["author"])
+    guess = ''
+    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+        f"Who said this quote? Guesses remaining: {remaining_guesses}"
+        guess = input()
+        if guess.lower() == quote["author"].lower():
+            print(f"Congrats you guessed it! It's {quote['author']}")
+            print("-")
+            # print(f"Here's a brief biography about {quote['author']} :\n")
+            # print("-")
+            # print(f"{soup.find(class_='author-description').split('.')[1:3]}...")
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get(f"{base_url}{quote['bio-link']}")
+            soup = BeautifulSoup(res.text, "html.parser")
+            birth_date = soup.find(class_="author-born-date").get_text()
+            birth_place = soup.find(class_="author-born-location").get_text()
+            print(f"Sorry no, here's a hint: Author was born on {birth_date} {birth_place}")
+        elif remaining_guesses == 2:
+            print(f"Sorry no, here's a hint: Author's first name starts with {quote['author'][0:2]}") #takes the first letter of the author's first name (first character of second list)
+        elif remaining_guesses == 1:
+            last_intital = quote['author'].split(" ")[1][0:2] #takes the first letter of the author's last name
+            print(f"Sorry no, here's a hint: Author's last name starts with {last_intital}")
+        else:
+            print
+            (f"Sorry you are out of guesses, it was {quote['author']}\n")
+            # print(f"Here's a brief biography about {quote['author']} :\n")
+            # print("-")
+            # print(f"{soup.find(class_='author-description').split('.')[1:3]}...")
+
+        again = ''
+    while again not in ('y', 'yes', 'n','no'):
+        again = input("Would you like to Play again? (y/n)")
+    if again.lower() in ('yes','y'):
+        return start_game()
+    else:
+
+
+        print("Thanks for playing!")
+        
+
+start_game()
+
